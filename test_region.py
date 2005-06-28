3eeccdbea573292dc18af1e86ad05ff512771eff
tjmather <tjmather>
2005-06-28 13:34:02 +0000

    changes for 1.2.1 release

--- test_region.py
+++ test_region.py
@@ -5,7 +5,11 @@ import GeoIP
 gi = GeoIP.open("/usr/local/share/GeoIP/GeoIPRegion.dat",GeoIP.GEOIP_STANDARD)
 
 gir = gi.region_by_name("www.google.com")
+if gir != None:
+            print gir['country_code']
+            print gir['region']
 
+gir = gi.region_by_addr("24.24.24.24")
 if gir != None:
             print gir['country_code']
             print gir['region']
