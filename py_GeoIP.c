cfba3b54f1318a6827509805f67dfba48f419c44
Boris Zentner <bzentner@maxmind.com>
2013-11-26 19:51:14 +0100

    Initialize everything to please -Wextra

--- py_GeoIP.c
+++ py_GeoIP.c
@@ -627,7 +627,36 @@ static PyTypeObject GeoIP_GeoIPType = {
     0,                         /*tp_as_buffer*/
     Py_TPFLAGS_DEFAULT,        /*tp_flags*/
     "GeoIP database object",   /* tp_doc */
-
+   0,                         /*  tp_traverse */
+   0,                         /*  tp_clear */
+   0,                         /*  tp_richcompare */
+   0,                         /*  tp_weaklistoffset */
+   0,                         /*  tp_iter */
+   0,                         /*  tp_iternext */
+   0,                         /*  tp_methods */
+   0,                         /*  tp_members */
+   0,                         /*  tp_getset */
+   0,               /*  tp_base */
+   0,                         /*  tp_dict */
+   0,                         /*  tp_descr_get */
+   0,                         /*  tp_descr_set */
+   0,                         /*  tp_dictoffset */
+   0,                         /*  tp_init */
+   0,                         /*  tp_alloc */
+   0,                         /*  tp_new */
+   0,                         /*  tp_free */
+   0,                         /*  tp_is_gc */
+   0,                         /*  tp_bases */
+   0,                         /*  tp_mro */
+   0,                         /*  tp_cache */
+   0,                         /*  tp_subclasses */
+   0                          /*  tp_weaklist */
+#if PY_MAJOR_VERSION >= 2 && PY_MINOR_VERSION >= 3
+   , 0                        /*  tp_del */
+#endif
+#if PY_MAJOR_VERSION >= 2 && PY_MINOR_VERSION >= 6
+   , 0                        /*  tp_version_tag */
+#endif
 };
 
 static PyMethodDef GeoIP_Class_methods[] = {
