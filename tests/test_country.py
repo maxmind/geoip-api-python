23919a30276d6402209e38d950d24de7def3e1a0
Gregory Oschwald <goschwald@maxmind.com>
2013-12-12 07:23:39 -0800

    Do the country test for different file modes

--- tests/test_country.py
+++ tests/test_country.py
@@ -3,10 +3,13 @@ from nose.tools import assert_equals
 
 
 def test_country():
-    gi = GeoIP.open("tests/data/GeoIP.dat", GeoIP.GEOIP_STANDARD)
+    for mode in [GeoIP.GEOIP_STANDARD, GeoIP.GEOIP_MEMORY_CACHE,
+                 GeoIP.GEOIP_MMAP_CACHE, GeoIP.GEOIP_INDEX_CACHE]:
+        gi = GeoIP.open("tests/data/GeoIP.dat", GeoIP.GEOIP_STANDARD)
 
-    assert_equals(gi.country_code_by_addr('64.17.254.216'), 'US')
-    assert_equals(gi.last_netmask(), 29)
-    assert_equals(gi.country_name_by_addr('64.17.254.216'), 'United States')
-    assert_equals(gi.range_by_ip('64.17.254.216'),
-                  ('64.17.254.216', '64.17.254.223'))
+        assert_equals(gi.country_code_by_addr('64.17.254.216'), 'US')
+        assert_equals(gi.last_netmask(), 29)
+        assert_equals(
+            gi.country_name_by_addr('64.17.254.216'), 'United States')
+        assert_equals(gi.range_by_ip('64.17.254.216'),
+                      ('64.17.254.216', '64.17.254.223'))
